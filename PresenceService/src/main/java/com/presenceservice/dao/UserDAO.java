89691e5751ad5f2ee03ab5c62000cf3dce1abaad
M. Cassany Gauthier <cassany@insa-11272>
2020-01-15 11:46:39 +0100

    PresenceService ongoing

--- PresenceService/src/main/java/com/presenceservice/dao/UserDAO.java
+++ PresenceService/src/main/java/com/presenceservice/dao/UserDAO.java
@@ -1,7 +1,16 @@
 package com.presenceservice.dao;
 
+import java.util.Optional;
+
+import com.presenceservice.model.User;
+import com.presenceservice.model.UserId;
+
 public interface UserDAO {
 	
-	//public Optional<User> getUser(UserId id) ; 
+	public Optional<User> getUser(UserId id) ; 
+	
+	public void addUser(User user) ; 
+	
+	public void updateUser(User user) ; 
 
 }
---
+++ PresenceService/src/main/java/com/presenceservice/dao/UserDAOSQLite.java
@@ -0,0 +1,28 @@
+package com.presenceservice.dao;
+
+import java.util.Optional;
+
+import com.presenceservice.model.User;
+import com.presenceservice.model.UserId;
+
+public class UserDAOSQLite implements UserDAO {
+
+	@Override
+	public Optional<User> getUser(UserId id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
